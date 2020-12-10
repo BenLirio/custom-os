94b1a30b87510c24de5e2690617ec12f2d6a9f0d
Ben Lirio <ben@bens-mbp.lan>
2020-12-09 20:03:44 -0500

    remove all files

--- hello-world-boot/emulate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-FILENAME="hello-world-boot"
-nasm "${FILENAME}".asm -f bin -o "${FILENAME}".bin
-qemu-system-x86_64 "${FILENAME}".bin
--- stack-boot/emulate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-nasm stack-boot.asm -f bin -o stack-boot.bin
-qemu-system-x86_64 stack-boot.bin
